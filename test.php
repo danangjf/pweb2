0c5d4b786a3b16bc6fd78b7f2fdbfd0dbf28fa2d
Danang Janan Fawwazan <80518749+danangjf@users.noreply.github.com>
2022-03-03 10:41:48 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+  $nim = "20090144";
+  $nama = "Danang Janan Fawwazan";
+  $kelas = "4B";
+    echo "My Biodata <br>
+    NIM   : $nim <br>
+    Nama  : $nama <br>
+    Kelas : $kelas <br>";
+
+ print("Semoga setelah mengenal kita bisa menyayangi sebab ada pepatah mengatakan 
+        - Tak kenal maka tak sayang -");
+  
+?>
